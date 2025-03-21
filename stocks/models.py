4592b4c08c9c86db6a14a0fc1692b106c5f01e09
Nikola Drcelic <nikola.drcelic11@gmail.com>
2025-03-21 22:47:06 +0100

    The logic for importing data into the database has been added #2

--- stocks/models.py
+++ stocks/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Stock(models.Model):
-    name = models.CharField(max_length=100)
-    date_of_creation = models.DateTimeField(auto_now_add=True)
-    abbreviation = models.CharField(max_length=5)
+    name = models.CharField(max_length=100, unique=True)
+    date_of_creation = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    abbreviation = models.CharField(max_length=5, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -12,9 +12,12 @@ class Stock(models.Model):
 class DatesValues(models.Model):
     date = models.DateField()
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    open = models.FloatField()
-    high = models.FloatField()
-    low = models.FloatField()
-    close = models.FloatField()
-    adj_close = models.FloatField()
-    volume = models.BigIntegerField()
+    open = models.FloatField(blank=True, null=True)
+    high = models.FloatField(blank=True, null=True)
+    low = models.FloatField(blank=True, null=True)
+    close = models.FloatField(blank=True, null=True)
+    adj_close = models.FloatField(blank=True, null=True)
+    volume = models.BigIntegerField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('date', 'stock')
